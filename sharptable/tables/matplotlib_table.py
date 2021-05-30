8f58c7c5c45dcd58e3f2ea404bae62f144c15988
Peter Bryan <peterbbryan@gmail.com>
2021-05-30 18:55:57 -0400

    Starting matplotlib table class

---
+++ sharptable/tables/matplotlib_table.py
@@ -0,0 +1,13 @@
+from sharptable.tables import Table
+
+
+class MatplotlibTable(Table):
+    """
+    Matplotlib based table.
+    """
+
+    def __init__(self):
+        """
+        """
+
+        ...
--- sharptable/tables/table.py
+++ sharptable/tables/table.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Table(ABC):
@@ -6,4 +6,21 @@ class Table(ABC):
     ABC for sharptable table types.
     """
 
-    ...
+    @abstractmethod
+    def show(self) -> None:
+        """
+        Show table in interactive sessions.
+        """
+
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_fig(self, path: str) -> None:
+        """
+        Save table to path.
+        
+        Args:
+            path: Path to write table to.
+        """
+
+        raise NotImplementedError
